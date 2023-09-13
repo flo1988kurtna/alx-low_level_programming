df6f7c4ec4e249b86f0cd536c3838d901820de5b
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-13 13:21:11 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+	char letter;
+	int i;
 
-    for (i = 0; i < 10; i++) /* Loop 10 times */
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
+	for (i = 0; i < 10; i++) /* Loop 10 times */
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
 
