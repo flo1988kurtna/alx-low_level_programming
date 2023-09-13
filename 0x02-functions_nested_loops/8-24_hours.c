7abfabbe99214f22d82c93533e7ea3259fae290b
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-13 14:48:41 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,19 +5,19 @@
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar('0' + (hour / 10)); /* Print the tens digit of the hour */
-            _putchar('0' + (hour % 10)); /* Print the ones digit of the hour */
-            _putchar(':');
-            _putchar('0' + (minute / 10)); /* Print the tens digit of the minute */
-            _putchar('0' + (minute % 10)); /* Print the ones digit of the minute */
-            _putchar('\n');
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + (hour / 10)); /* Print the tens digit of the hour */
+			_putchar('0' + (hour % 10)); /* Print the ones digit of the hour */
+			_putchar(':');
+			_putchar('0' + (minute / 10)); /* Print the tens digit of the minute */
+			_putchar('0' + (minute % 10)); /* Print the ones digit of the minute */
+			_putchar('\n');
+		}
+	}
 }
 
