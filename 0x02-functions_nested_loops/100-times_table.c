98b1e4631b542aeae711d2c23c902117ca84d385
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-13 16:17:11 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,51 +6,43 @@
  */
 void print_times_table(int n)
 {
-    int row, column, product;
+	int row, column, product;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (row = 0; row <= n; row++)
-    {
-        for (column = 0; column <= n; column++)
-        {
-            product = row * column;
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			product = row * column;
 
-            if (column == 0)
-                _putchar('0');
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (column == 0)
+				_putchar('0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (product < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else if (product < 100)
-                {
-                    _putchar(' ');
-                }
-            }
+				if (product < 10)
+					_putchar(' ');
+			}
 
-            if (product >= 100)
-                _putchar(product / 100 + '0');
+			if (product < 10)
+				_putchar(' ');
 
-            if (product >= 10)
-                _putchar((product / 10) % 10 + '0');
+			if (product >= 100)
+				_putchar(product / 100 + '0');
 
-            _putchar(product % 10 + '0');
+			if (product >= 10)
+				_putchar((product / 10) % 10 + '0');
 
-            /* Print a newline character after the last column */
-            if (column == n)
-                _putchar('\n');
-        }
-    }
+			_putchar(product % 10 + '0');
+
+			/* Print a newline character after the last column */
+			if (column == n)
+				_putchar('\n');
+		}
+	}
 }
-
-				    
-				    
-				    
 				    
