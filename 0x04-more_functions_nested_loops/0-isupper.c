8981ca9073eaca84521606fbfc9c8735b67624ed
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-15 08:06:33 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 
 /**
  * _isupper - Checks if a character is uppercase.
- * @c: The character to check.
+ * @c: The character to be checked.
  *
- * Return: 1 if `c` is uppercase, 0 otherwise.
+ * Return: 1 if the character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
