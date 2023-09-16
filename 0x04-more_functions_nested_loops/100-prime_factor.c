9a364f5497bff5259b229dc23489aa6b1bba8e96
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-15 18:15:55 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,36 +9,36 @@
  */
 long largest_prime_factor(long n)
 {
-    long max_prime = -1;
-    
-    while (n % 2 == 0)
-    {
-        max_prime = 2;
-        n /= 2;
-    }
-    
-    for (long i = 3; i <= sqrt(n); i += 2)
-    {
-        while (n % i == 0)
-        {
-            max_prime = i;
-            n /= i;
-        }
-    }
-    
-    if (n > 2)
-        max_prime = n;
-    
-    return max_prime;
+	long max_prime = -1;
+
+	while (n % 2 == 0)
+	{
+		max_prime = 2;
+		n /= 2;
+	}
+
+	for (long i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			max_prime = i;
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+		max_prime = n;
+
+	return (max_prime);
 }
 
 int main(void)
 {
-    long number = 612852475143;
-    long result = largest_prime_factor(number);
-    
-    printf("%ld\n", result);
-    
-    return (0);
+	long number = 612852475143;
+	long result = largest_prime_factor(number);
+
+	printf("%ld\n", result);
+
+	return (0);
 }
 
