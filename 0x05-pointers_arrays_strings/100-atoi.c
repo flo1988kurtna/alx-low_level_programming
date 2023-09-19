e923a47a0eb52bfdfee32155055bbaa91452c4f4
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 06:48:09 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -8,21 +9,35 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			result = result * 10 + (s[i] - '0');
-		else if (result > 0)
-			break;
-		i++;
-	}
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] >= '0' && s[i] <= '9')
+            result = result * 10 + (s[i] - '0');
+        else if (result > 0)
+            break;
+        i++;
+    }
 
-	return (result * sign);
+    return (result * sign);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int n;
+
+    n = _atoi("-2147483648");
+    printf("%d\n", n);
+    return (0);
 }
 
