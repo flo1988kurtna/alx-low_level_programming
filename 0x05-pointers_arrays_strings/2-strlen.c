6ec9c3553952627f97e471f312abb9baec9b0822
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 04:12:00 -0700

    This report, by its very length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: A pointer to a string.
+ * @s: A pointer to a string.
+ *
  * Return: The length of the string.
  */
-size_t _strlen(const *str)
-
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int length = 0;
 
 	while (*s != '\0')
 	{
