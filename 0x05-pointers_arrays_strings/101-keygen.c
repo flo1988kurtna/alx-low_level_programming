b4c67b62bcb1955acd4537a07a39f223e8cbd476
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 11:05:46 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
 
-#define PASSWORD_LENGTH 12  // Adjust this as needed
+#define PASSWORD_LENGTH 12
 
-char generateRandomChar() {
-    // Characters allowed in the password
-    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int charset_length = sizeof(charset) - 1;
+int main(void)
+{
+    int i;
+    char password[PASSWORD_LENGTH + 1]; /* +1 for the null terminator */
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    // Generate a random index within the charset
-    int random_index = rand() % charset_length;
-
-    // Return the randomly selected character
-    return charset[random_index];
-}
-
-int main() {
-    // Seed the random number generator with the current time
+    /* Seed the random number generator with the current time */
     srand(time(NULL));
 
-    // Generate the password
-    char password[PASSWORD_LENGTH + 1];  // +1 for the null terminator
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = generateRandomChar();
+    /* Generate a random password */
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
     }
     password[PASSWORD_LENGTH] = '\0';
 
-    // Output the generated password
+    /* Print the generated password */
     printf("%s\n", password);
 
     return 0;
