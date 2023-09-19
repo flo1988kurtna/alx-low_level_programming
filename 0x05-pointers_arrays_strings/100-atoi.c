238e2e941d71173001fd4dccc236b9c7d702532e
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 09:47:59 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,51 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: The pointer to convert
- * Return: A integer
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The integer value of the string.
  */
-
 int _atoi(char *s)
-
 {
+	int sign = 1;
+	int result = 0;
+	int digit;
 
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi =0;
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = *s - '0';
 
-	while (s[c])
-	{
-	if (s[c] == 45)
-	{
-	min *= -1;
+			/* Check for integer overflow */
+			if (result > (INT_MAX - digit) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
+			result = result * 10 + digit;
+		}
+		else if (*s == '+')
+		{
+			/* Handle the '+' sign */
+		}
+		else if (result != 0)
+		{
+			/* Stop parsing if non-digit characters appear after a number */
+			break;
+		}
+
+		s++;
 	}
-	while (s[c] >= 48 && s[c] <= 57)
-	}
-	isi = 1;
-	ni = (ni *10) +(s[c] - '0');
-	c++;
-	}
-	if (isi == 1)
-	{
-	break;
-	}
-	c++
-	}
-	ni *=min;
-	return (n);
+
+	return (result * sign);
 }
+
