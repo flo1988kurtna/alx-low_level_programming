1ee53cddaca96c01231d850f8d14eb13e3c89356
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 06:35:32 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,15 +11,31 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int i = 0;
+	int has_digit = 0;  /* Flag to track if a valid digit is encountered */
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
+		{
+			if (has_digit)  /* Stop parsing if '-' is encountered after digits */
+				break;
 			sign *= -1;
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			has_digit = 1;
+			/* Check for integer overflow before updating result */
+			if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+			{
+				/* Handle overflow, return INT_MIN if negative, INT_MAX if positive */
+				return (sign == -1) ? INT_MIN : INT_MAX;
+			}
 			result = result * 10 + (s[i] - '0');
-		else if (result > 0)
-			break;
+		}
+		else if (has_digit)
+		{
+			break;  /* Stop parsing after encountering non-digit after digits */
+		}
 		i++;
 	}
 
