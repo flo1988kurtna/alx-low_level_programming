b2e20c87fa068b1e0cee64c57d4a9066e901784c
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 09:57:58 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,21 +8,30 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			result = result * 10 + (s[i] - '0');
-		else if (result > 0)
-			break;
-		i++;
-	}
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            if (result > 0 && (result * 10 + (s[i] - '0')) < result)
+            {
+                if (sign == -1)
+                    return (-2147483648);
+                else
+                    return (2147483647);
+            }
+            result = result * 10 + (s[i] - '0');
+        }
+        else if (result > 0)
+            break;
+        i++;
+    }
 
-	return (result * sign);
+    return (result * sign);
 }
 
