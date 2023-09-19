e999268e34c9e8b4f914a1fdd95459337a8ed45b
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 05:35:39 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,19 +8,19 @@
  */
 void puts2(char *str)
 {
-    int i;
-    int j = 0;
+	int i;
+	int j = 0;
 
-    while (str[j] != '\0')
-    {
-        j++;
-    }
+	while (str[j] != '\0')
+	{
+		j++;
+	}
 
-    for (i = 0; i < j; i += 2)
-    {
-        _putchar(str[i]);
-    }
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
-	
+
