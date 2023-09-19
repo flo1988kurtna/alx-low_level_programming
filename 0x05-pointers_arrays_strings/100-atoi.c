d8b538ca2a3eb4d7bc986adf65cbf0be8ca97c34
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-19 10:46:15 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer.
+ * _atoi - Converts a string
+ * to an integer.
  * @s: The input string.
  *
- * Return: The integer 
+ * Return: The integer value 
+ * converted from the string.
  */
 int _atoi(char *s)
 {
