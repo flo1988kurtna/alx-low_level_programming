eb3c07583a429a0fffc16e1a92ff62054378a342
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-21 02:16:45 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts all lowercase letters in a string to uppercase
+ * @str: The input string
+ *
+ * Return: A pointer to the resulting string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -= ('a' - 'A');
+		}
+		ptr++;
+	}
+
+	return str;
+}
+
