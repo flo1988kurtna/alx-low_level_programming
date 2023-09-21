f93aaa01382d7705eac6bdeb0d3154fdea685b1a
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-21 03:48:12 -0700

    A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,24 @@
 #include "main.h"
-#include <unistd.h> /* Include the necessary header for the write function */
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to print
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  *
- * Return: On success 1, on error -1
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-/**
- * print_number - Prints an integer
- * @n: The integer to print
+ * Description: This function prints an integer to the standard output
+ * using only the _putchar function. It handles both positive and negative
+ * integers and does not use long, arrays, or pointers.
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
+    if (n < 0) {
         _putchar('-');
         n = -n;
     }
 
-    if (n / 10 != 0)
-    {
+    if (n / 10 != 0) {
         print_number(n / 10);
     }
 
-    _putchar(n % 10 + '0');
+    putchar((n % 10) + '0');
 }
+
