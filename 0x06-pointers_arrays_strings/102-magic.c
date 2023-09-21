db9fd07a9908e3e328e69f53e3756284f5b8070a
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-21 08:09:54 -0700

    A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: This is the main function that serves as the entry point
- *              for the program.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int n;
+  int n;
+  int a[5];
+  int *p;
 
-	/*
-	 * Your code goes here...
-	 * Remember:
-	 * - You are not allowed to use any variable other than n
-	 * - You are not allowed to use any other line of code
-	 */
-	n = 98; /* Your code here */
-
-	printf("n is %d\n", n);
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p+5)  98
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
-
