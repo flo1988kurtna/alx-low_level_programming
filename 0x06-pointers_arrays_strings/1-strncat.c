b11406b856784e33adc66677b885c91d1d7a609a
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-21 01:19:28 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes from src to concatenate
+ *
+ * Return: A pointer to the resulting string `dest`
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	/* Find the length of dest */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	/* Concatenate src to dest, using at most n bytes */
+	while (src[i] != '\0' && i < n)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+
+	dest[dest_len] = '\0'; /* Null-terminate the concatenated string */
+
+	return dest;
+}
+
