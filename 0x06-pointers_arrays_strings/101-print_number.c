4d87679d23038c2e85773972bb9d4ab21ce97391
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-21 07:27:59 -0700

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,23 +2,23 @@
 
 /**
  * print_number - Prints an integer.
- * @n: The integer to be printed.
- *
- * Description: This function prints an integer to the standard output
- * using only the _putchar function. It handles both positive and negative
- * integers and does not use long, arrays, or pointers.
+ * @n: integer params
+ * Return: 0
  */
+
 void print_number(int n)
 {
-    if (n < 0) {
+    if (n < 0)
+    {
         _putchar('-');
         n = -n;
     }
 
-    if (n / 10 != 0) {
+    if (n / 10 != 0)
+    {
         print_number(n / 10);
     }
 
-    putchar((n % 10) + '0');
+    _putchar((n % 10) + '0');
 }
 
