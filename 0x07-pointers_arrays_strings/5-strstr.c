c2f6895d935adffb8dd970b1f20a8fe7b767a2ca
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-25 11:09:36 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,23 +11,23 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-        }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
 
-        if (needle[j] == '\0')
-            return (haystack + i);
-    }
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
