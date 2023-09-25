58239d1e5fe6dfdc621f3cba49b77d76aa1455be
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-25 11:51:10 -0700

    The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagnosis - Entry point
+ * @a: input
+ * @size: input 
+ * Return: Always 0 (success)
+ */
+
+void print_diagsums(int *a, int size)
+{
+    int sum1 = 0;
+    int sum2 = 0;
+    int i;
+
+    for (i = 0; i < size; i++) {
+        /* Calculate the sum of the main diagonal */
+        sum1 += *(a + i * size + i);
+
+        /* Calculate the sum of the secondary diagonal */
+        sum2 += *(a + i * size + (size - 1 - i));
+    }
+
+    printf("%d, %d\n", sum1, sum2);
+}
+
