b6a2874da683056f5d230859628a1a8815a07823
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-25 11:03:21 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,27 +7,27 @@
  * @needle: The substring to search for.
  *
  * Return: Pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ *         or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+    int i, j;
 
-	if (*needle == '\0')
-		return haystack;
+    if (*needle == '\0')
+        return (haystack);
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
+    for (i = 0; haystack[i] != '\0'; i++)
+    {
+        for (j = 0; needle[j] != '\0'; j++)
+        {
+            if (haystack[i + j] != needle[j])
+                break;
+        }
 
-		if (needle[j] == '\0')
-			return (haystack + i);
-	}
+        if (needle[j] == '\0')
+            return (haystack + i);
+    }
 
-	return NULL;
+    return (NULL);
 }
 
