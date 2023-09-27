e3b79aa8ece824b702c71ec73d1e0e487ead5961
Florence Mkombola <fnathaniel471@gmail.com>
2023-09-27 01:30:19 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: The string to be printed
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') /* Check if we have reached the end of the string (null terminator) */
+    {
+        _putchar('\n'); /* Print a newline character to complete the line */
+        return;
+    }
+
+    _putchar(*s); /* Print the current character */
+    _puts_recursion(s + 1); /* Move to the next character in the string */
+}
+
+
